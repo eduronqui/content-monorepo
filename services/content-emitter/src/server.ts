7637c76b27b8f17a86dc8c8e67f79d9724c11ca1
Eduardo Ronqui da Silva <edu.ronqui@gmail.com>
2022-03-29 19:59:24 +0200

    feat: add kafka producer poc

--- services/content-emitter/src/server.ts
+++ services/content-emitter/src/server.ts
@@ -1,8 +1,11 @@
 import { randomUUID } from 'crypto'
-import Fastify from 'fastify'
+import Fastify, { fastify } from 'fastify'
 import { bootstrapPlugins } from './plugins'
 import { bootstrapHooks } from './hooks'
 import { bootstrapHandlers } from './handlers'
+import { KafkaClient } from './clients'
+import config from 'config'
+import fp from 'fastify-plugin'
 
 export async function bootstrap() {
   const server = Fastify({
@@ -14,13 +17,50 @@ export async function bootstrap() {
   server.register(bootstrapHooks())
   server.register(bootstrapHandlers())
 
-  server.get('/', async (request, reply) => {
-    reply.type('application/json').code(200)
-
-    return { hello: 'world' }
+  const kafkaConfig = config.get<{ clientId: string; brokers: string[] }>('kafka')
+  const kafka = KafkaClient(kafkaConfig)
+  const kafkaProducer = kafka.producer({
+    retry: {
+      initialRetryTime: 1000,
+      retries: 5
+    }
   })
 
-  server.listen(3000)
+  server.addHook('onReady', async () => {
+    await kafkaProducer.connect()
+  })
+
+  server.addHook('onClose', async () => {
+    await kafkaProducer.disconnect()
+  })
+
+  server.get('/', async (request, reply) => {
+    try {
+      const response = await kafkaProducer.send({
+        topic: 'hello-world-topic',
+        messages: [
+          {
+            key: 'hello-world-message',
+            headers: {
+              'correlation-id': request.headers['correlation-id']?.toString()
+            },
+            value: JSON.stringify({ hello: 'world' })
+          }
+        ]
+      })
+      request.log.info({ response }, 'Message sent')
+    } catch (error) {
+      request.log.error({ error }, 'Failed to post kafka message')
+
+      reply.type('application/json').code(500)
+      return (error as Error).message
+    }
+
+    reply.type('application/json').code(202)
+    return 'Accepted'
+  })
+
+  await server.listen(3000)
 
   return server.ready()
 }

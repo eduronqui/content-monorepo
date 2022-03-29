059e01730a3ad8ecd3c786a99b0dace48d8a9eca
Eduardo Ronqui da Silva <edu.ronqui@gmail.com>
2022-03-29 23:34:17 +0200

    refactor: use @cts/correlation-id package on content-emitter
    fix #3

--- services/content-emitter/src/hooks/index.ts
+++ services/content-emitter/src/hooks/index.ts
@@ -1,10 +1,8 @@
 import fp from 'fastify-plugin'
 import { onRequest } from './onRequest'
-import { onSend } from './onSend'
 
 export function bootstrapHooks() {
   return fp(async (fastify) => {
     fastify.addHook('onRequest', onRequest)
-    fastify.addHook('onSend', onSend)
   })
 }
--- services/content-emitter/src/hooks/onRequest.ts
+++ services/content-emitter/src/hooks/onRequest.ts
@@ -1,12 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import config from 'config'
 
-const CORRELATION_ID = 'correlation-id'
-
 export async function onRequest(request: FastifyRequest, reply: FastifyReply) {
-  if (!request.headers[CORRELATION_ID]) {
-    request.headers[CORRELATION_ID] = request.id
-  }
-
-  request.config = config
+  (request as any).config = config
 }

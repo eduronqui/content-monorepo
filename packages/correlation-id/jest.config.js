aa9d6bca7efeb36c61ecef97f2bf5f1932671e31
Eduardo Ronqui da Silva <edu.ronqui@gmail.com>
2022-03-29 22:06:53 +0200

    feat(@cts/correlation-id): extract @cts/correlation-id to its own package
    re #3

---
+++ packages/correlation-id/jest.config.js
@@ -0,0 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const jestConfig = require('../../jest.config')
+
+module.exports = jestConfig

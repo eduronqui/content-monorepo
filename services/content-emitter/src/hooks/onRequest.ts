1d78ce663913176669cc0ecfcbeb499be5714b47
Eduardo Ronqui da Silva <edu.ronqui@gmail.com>
2022-03-30 09:03:45 +0200

    refactor(content-emitter): fix config types

--- services/content-emitter/src/hooks/onRequest.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
-import config from 'config'
-
-export async function onRequest(request: FastifyRequest, reply: FastifyReply) {
-  (request as any).config = config
-}
